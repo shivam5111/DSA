920e105515c9d3e7d4c2e8c98c52cec5bbfb653e
shivam5111 <112190086+shivam5111@users.noreply.github.com>
2022-10-17 11:30:34 +0530

    assignment

---
+++ assignment 5/insert head.cpp
@@ -0,0 +1,17 @@
+// Complete the insertNodeAtHead function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* insertNodeAtHead(SinglyLinkedListNode* llist, int data) {
+SinglyLinkedListNode* n= new SinglyLinkedListNode(data);
+n->next= llist;
+return llist=n;
+
+}
